59f87d5daa8ecee2f0ed6b3b21c5eb81fabb7cd4
WovenTexLTD <karimsabbagh@woventex.co>
2025-08-28 10:39:07 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
+import { initAnalytics } from "./analytics";
+// …
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+initAnalytics(); // will no-op until consent exists
