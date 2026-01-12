89f0feb1fa418d0d97b904d6ef0cfbe152f20102
Karim Sabbagh <karimsabbagh@woventex.co>
2026-01-12 17:07:07 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -102,6 +102,7 @@ function App() {
             <Route path="/sustainability" element={<Sustainability />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           </Routes>
         </main>
 
